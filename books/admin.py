d03aaac3a8f9d73c19671c620205de85cf11f35b
unknown <rhystyler.cardiff@gmail.com>
2025-02-26 19:13:15 +0000

    Update books model to fetch open libary date

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import Category, Books
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+class BooksAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'author',
+        'category',
+        'realised_date',
+    )
+    ordering = ('category',)
+
 admin.site.register(Category)
 admin.site.register(Books)
--- books/models.py
+++ books/models.py
@@ -1,6 +1,10 @@
+import requests
 from django.db import models
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -12,14 +16,40 @@ class Category(models.Model):
     
 class Books(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    isbn = models.CharField(max_length=254, null=True, blank=True)
+    isbn = models.CharField(max_length=13, null=True, blank=True, unique=True)
     sku = models.CharField(max_length=254, null=True, blank=True)
     title = models.CharField(max_length=254)
-    author = models.CharField(max_length=254)
-    realised_date = models.DateField()
-    description = models.TextField()
+    author = models.CharField(max_length=254, null=True, blank=True)
+    realised_date = models.DateField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.title
+
+    def fetch_book_details(self):
+        """
+        Fetches book details from Open Library API using ISBN
+        """
+        if not self.isbn:
+            return
+
+        url = f"https://openlibrary.org/api/books?bibkeys=ISBN:{self.isbn}&format=json&jscmd=data"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json().get(f"ISBN:{self.isbn}", {})
+
+            if data:
+                self.title = data.get("title", self.title)
+                self.author = ", ".join([author["name"] for author in data.get("authors", [])])
+                self.realised_date = data.get("publish_date", None)  # Open Library provides a string
+                self.description = data.get("description", {}).get("value", "")
+                cover = data.get("cover", {}).get("large") or data.get("cover", {}).get("medium")
+                self.image_url = cover if cover else self.image_url
+
+    def save(self, *args, **kwargs):
+        if not self.title or not self.author:  # Only fetch if details are missing
+            self.fetch_book_details()
+        super().save(*args, **kwargs)
