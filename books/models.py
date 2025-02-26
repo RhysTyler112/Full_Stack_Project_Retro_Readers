6d02b74b77bbad2d28efe8abab8f0b0ec1bf0990
unknown <rhystyler.cardiff@gmail.com>
2025-02-26 18:44:35 +0000

    Added books app, model and fixture

---
+++ books/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+    
+class Books(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    isbn = models.CharField(max_length=254, null=True, blank=True)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    title = models.CharField(max_length=254)
+    author = models.CharField(max_length=254)
+    realised_date = models.DateField()
+    description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
