a4e3495f0b827afdb0a1193aff46786efd20ef35
unknown <rhystyler.cardiff@gmail.com>
2025-03-01 14:58:51 +0000

    Added filtering by catergory and book format

--- books/views.py
+++ books/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Books
+from .models import Books, Category
 
 # Create your views here.
 
@@ -9,8 +9,14 @@ def book_list(request):
     """A view that displays all books, including sorting and search queries"""
     books = Books.objects.all()
     query = None
+    categories = None
     
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            books = books.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+        
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -20,9 +26,20 @@ def book_list(request):
             queries = Q(title__icontains=query) | Q(description__icontains=query) 
             books = books.filter(queries)
     
+    format_filter = request.GET.get('format')
+    if format_filter == 'softcover':
+        books = Books.objects.filter(price_softcover__isnull=False)
+    elif format_filter == 'hardcover':
+        books = Books.objects.filter(price_hardcover__isnull=False)
+    elif format_filter == 'audiobook':
+        books = Books.objects.filter(price_audiobook__isnull=False)
+    else:
+        books = Books.objects.all()
+        
     context = {
         'books': books,
         'search_term': query,
+        'current_categories': categories,
     }
     
     return render(request, "books/books.html", context)
