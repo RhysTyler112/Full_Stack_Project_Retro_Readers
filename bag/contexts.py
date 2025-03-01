ad1660556a83f97bac3ab0d015a40ca3a289f481
unknown <rhystyler.cardiff@gmail.com>
2025-03-01 20:39:33 +0000

    Updated context process

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,11 +1,37 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from books.models import Books
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, formats in bag.items():
+        book = get_object_or_404(Books, isbn=item_id)
+        for format, quantity in formats.items():
+            # Determine the price based on the format
+            if format == 'softcover':
+                price = book.price_softcover
+            elif format == 'hardcover':
+                price = book.price_hardcover
+            elif format == 'audiobook':
+                price = book.price_audiobook
+            else:
+                price = 0  # Default to 0 if no valid format is found
+
+            total += quantity * price
+            product_count += quantity
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': quantity,
+                'book': book,
+                'format': format,
+                'price': price,
+            })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- bag/views.py
+++ bag/views.py
@@ -1,24 +1,29 @@
-from django.shortcuts import render, redirect
-
-# Create your views here.
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from books.models import Books
 
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
-def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
-
+def add_to_bag(request, isbn):
+    """Add a quantity of the specified book to the shopping bag"""
+    book = get_object_or_404(Books, isbn=isbn)
     quantity = int(request.POST.get('quantity'))
+    format = request.POST.get('format')
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if isbn in bag:
+        if format in bag[isbn]:
+            bag[isbn][format] += quantity
+        else:
+            bag[isbn][format] = quantity
     else:
-        bag[item_id] = quantity
+        bag[isbn] = {format: quantity}
 
     request.session['bag'] = bag
-    print(request.session['bag'])
+    messages.success(request, f'Added {book.title} ({format}) to your bag')
+
     return redirect(redirect_url)
