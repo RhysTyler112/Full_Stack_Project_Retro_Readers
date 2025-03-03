c42019c14de1cf0fcc7f1dd8eaef341f74c9585f
unknown <rhystyler.cardiff@gmail.com>
2025-03-03 20:44:28 +0000

    Added remove and adjcut bag views, and the calcs for subtotal

--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<str:isbn>/', views.add_to_bag, name='add_to_bag'),
+    path('adjust/<str:isbn>/', views.adjust_bag, name='adjust_bag'),
+    path('remove/<str:isbn>/', views.remove_from_bag, name='remove_from_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.contrib import messages
 from books.models import Books
 
@@ -27,3 +27,49 @@ def add_to_bag(request, isbn):
     messages.success(request, f'Added {book.title} ({format}) to your bag')
 
     return redirect(redirect_url)
+
+def adjust_bag(request, isbn):
+    """Adjust the quantity of the specified book in the shopping bag"""
+    book = get_object_or_404(Books, isbn=isbn)
+    quantity = int(request.POST.get('quantity'))
+    format = request.POST.get('format')
+    bag = request.session.get('bag', {})
+
+    if format:
+        if quantity > 0:
+            bag[isbn][format] = quantity
+        else:
+            del bag[isbn][format]
+            if not bag[isbn]:
+                bag.pop(isbn)
+    else:
+        if quantity > 0:
+            bag[isbn] = quantity
+        else:
+            bag.pop(isbn)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, isbn):
+    """Remove the specified book out of the shopping bag"""
+    book = get_object_or_404(Books, isbn=isbn)
+    format = request.POST.get('format')
+    bag = request.session.get('bag', {})
+
+    try:
+        if isbn in bag:
+            if format and format in bag[isbn]:
+                del bag[isbn][format]
+                if not bag[isbn]:
+                    bag.pop(isbn)
+            elif not format:
+                bag.pop(isbn)
+            request.session['bag'] = bag
+            return HttpResponse(status=200)
+        else:
+            return HttpResponse(status=404)
+    
+    except Exception as e:
+        return HttpResponse(status=500)
