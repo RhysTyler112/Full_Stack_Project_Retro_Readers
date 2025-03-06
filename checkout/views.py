96ffb497adc243b03ca987a2e6989955a588874e
unknown <rhystyler.cardiff@gmail.com>
2025-03-06 17:32:38 +0000

    Added stripe elemets to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QpDDVLhLoaWpDIt0KfRuRX3CuHibfapgR0jx4fjXoNSLhbRpr27pbeTgIApn16x85XZHDoTq7PVfavAjYouLqM400MFwc5eZS',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
