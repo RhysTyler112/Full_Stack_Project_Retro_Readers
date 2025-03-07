16f5d53441cd2f07ae54c83b0897022f34a54493
unknown <rhystyler.cardiff@gmail.com>
2025-03-07 20:06:53 +0000

    Added logic for succesful checkout

--- checkout/models.py
+++ checkout/models.py
@@ -53,12 +53,12 @@ class Order(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return super().order_number
+        return self.order_number
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     book = models.ForeignKey(Books, null=False, blank=False, on_delete=models.CASCADE)
-    format = models.CharField(max_length=6, null=False, blank=False)
+    format = models.CharField(max_length=9, null=False, blank=False)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
     
